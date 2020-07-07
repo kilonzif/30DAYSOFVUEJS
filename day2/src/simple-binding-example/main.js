25372d8062c6cb36082563bff4041d6e350ec0db
FaithK <faith.kilonzi@makeduconsult.com>
2020-07-07 00:40:07 +0000

    added day3 files on list rendering using v-for and key

--- day2/src/simple-binding-example/main.js
+++ day2/src/simple-binding-example/main.js
@@ -18,16 +18,5 @@ new Vue({
    }
     }
   }
-  //   methods: {
-  //   changeCity() {
-  //     if (this.city === 'Accra') {
-  //       this.city = 'Nairobi';
-  //       this.imagesrc: 'https://www.perseuss.com/_contentediting/files/51c14fa7dc2d410688ee3c7a095e7ce0.jpg';
-  //     } else {
-  //       this.city = 'Accra';
-  //       thisimagesrc: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Independence_Square%2C_Accra%2C_Ghana.JPG/400px-Independence_Square%2C_Accra%2C_Ghana.JPG';
  
-  //     }
-  //   }
-  // }
 });
---
+++ day3/README.md
@@ -0,0 +1,3 @@
+<h3>Day 03 - The Vue Instance - Data Driven Applications</h3>
+
+Application can be run by opening the `src/index.html` file in your favourite browser.
